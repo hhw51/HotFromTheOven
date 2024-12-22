c1606d741085f30ebcbd1a9d61d4666ac000293a
hhw51 <harishassanwyne@gmail.com>
2024-12-23 01:23:07 +0500

    hero

--- src/app/components/AllProducts/SectionTitle.tsx
+++ src/app/components/AllProducts/SectionTitle.tsx
@@ -6,7 +6,7 @@ const SectionTitle: React.FC = () => {
       <h2 className="text-sm font-bold uppercase text-orange-500">Our Shop</h2>
       <h1 className="text-4xl font-bold mt-4">All Products</h1>
       <p className="mt-6 text-gray-600">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus urna a dictum neque molestie ultricies.
+        Discover a world of freshly baked delights, crafted to bring joy to every bite.
       </p>
     </div>
   );
